2cf7155cb7ac44f10563800ee66ac72078d85e56
tonyjhuang <huangtonyj91@gmail.com>
2018-10-30 10:04:10 -0700

    Set up env: yarn init, index.html, index.css, index.js, webpack + webpack.config.js, packages

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('hello from index.js');
+const h1 = document.getElementById('test');
+
+document.addEventListener('click', () => alert('hello, webpack is running'))
+
+
+console.log('testing webpack bundle.js');
