74bee27d24a490b373b749e3544f2b612c3c64e7
tonyjhuang <huangtonyj91@gmail.com>
2018-10-30 20:41:37 -0700

    Refactor ball into Ball class

--- scripts/ball.js
+++ scripts/ball.js
@@ -1,9 +1,49 @@
+class Ball {
+  constructor(options) {
+    this.canvas = options.canvas;
+    this.context = this.canvas.getContext('2d');
+    this.x = options.x;
+    this.y = options.y;
 
+    this.ballVelocity = 3;
 
-// class Ball {
-//   constructor() {
-//     this.radius = 10;
-//     this.color = red;
+    this.dx = this.ballVelocity;
+    this.dy = -this.ballVelocity;
 
-//   }
-// }
+    this.ballRadius = 10;
+    this.fillStyle = 'green';
+    this.strokeStyle = 'black';
+  }
+
+  update() {
+    this.clearBallPath();
+
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2, false);
+
+    this.context.fillStyle = this.fillStyle;
+    this.context.fill();
+    
+    this.context.strokeStyle = this.strokeStyle;
+    this.context.stroke();
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if ((this.x > this.canvas.width - this.ballRadius) || (this.x < this.ballRadius)) {
+      this.dx *= -1;      
+    }
+
+    if ((this.y > this.canvas.height - this.ballRadius) || (this.y < this.ballRadius)) {
+      this.dy *= -1;
+    }
+  }
+
+  clearBallPath() {
+    // Need a more elaborate algo
+    this.context.clearRect(this.x - 15, this.y - 15, this.ballRadius * 3, this.ballRadius * 3)
+  }
+
+}
+
+module.exports = Ball;
--- scripts/breakout.js
+++ scripts/breakout.js
@@ -1,4 +1,4 @@
-require('./ball.js')
+const Ball = require('./ball');
 
 const canvas = document.getElementById('canvasRoot');
 // canvas.width = window.innerWidth;
@@ -6,7 +6,7 @@ const canvas = document.getElementById('canvasRoot');
 canvas.width = 1000;
 canvas.height = 700;
 
-const c = canvas.getContext('2d');
+const canvasContext = canvas.getContext('2d');
 
 // Platform
 const platformWidth = 150;
@@ -14,42 +14,24 @@ const platformHeight = 15;
 let platformX = (canvas.width - platformWidth) / 2;
 let platformY = (canvas.height - platformHeight - 15);
 
-c.beginPath();
-c.fillRect(platformX, platformY, platformWidth, platformHeight)
+canvasContext.beginPath();
+canvasContext.fillRect(platformX, platformY, platformWidth, platformHeight)
 
 
 // Ball
 const ballRadius = 10;
-const velocity = 1;
-let x = platformX;
-let y = platformY;
-let dx = velocity;
-let dy = -velocity;
+const ballVelocity = 1;
+
+const ball = new Ball({
+  canvas: canvas,
+  x: platformX + (platformWidth/2),
+  y: platformY - platformHeight
+})
 
 function animate () {
   requestAnimationFrame(animate);
-  
-  // clearing helper method!, dependent on speed
-  // c.clearRect(0, 0, canvas.width, canvas.height)
-  c.clearRect(x - 15, y - 15, ballRadius * 3, ballRadius * 3)
-  
-  c.beginPath();
-  c.arc(x, y, ballRadius, 0, Math.PI * 2, false);
-  c.strokeStyle = 'black'
-  c.stroke();
-  c.fillStyle = 'green';
-  c.fill();
 
-  x += dx;
-  y += dy;
-
-  if ((x > canvas.width - ballRadius) || (x < ballRadius)) {
-    dx *= -1;
-  }
-
-  if ((y > canvas.height - ballRadius) || (y < ballRadius)) {
-    dy *= -1;
-  }
+  ball.update();
 
 }
 
