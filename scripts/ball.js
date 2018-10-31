9cf088ecb4f059b41f06a13833f753bb01f13345
tonyjhuang <huangtonyj91@gmail.com>
2018-10-31 10:50:40 -0700

    Create Ball.resetBall() position and incorporate previously refactored Platform class

--- scripts/ball.js
+++ scripts/ball.js
@@ -1,50 +1,48 @@
 class Ball {
-  constructor(options) {
-    this.canvas = options.canvas;
-    this.x_i = options.x_i;
-    this.y_i = options.y_i;
-
+  constructor(canvas, platform) {
+    this.canvas = canvas;
     this.context = this.canvas.getContext('2d');
+
+    this.platform = platform;
     
     this.ballVelocity = 3;
-
-    this.dx = this.ballVelocity;
-    this.dy = -this.ballVelocity;
-    this.x = this.x_i;
-    this.y = this.y_i;
-
-    this.ballRadius = 10;
-    this.fillStyle = 'green';
+    this.ballRadius = 8;
+    this.fillStyle = 'orange';
     this.strokeStyle = 'black';
+
+    this.resetBall();
   }
 
   update() {
-    this.clearBallPath();
+    // this.clearBallPath();
 
     this.context.beginPath();
     this.context.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2, false);
 
     this.context.fillStyle = this.fillStyle;
     this.context.fill();
-    
     this.context.strokeStyle = this.strokeStyle;
     this.context.stroke();
 
     this.x += this.dx;
     this.y += this.dy;
 
-    if ((this.x > this.canvas.width - this.ballRadius) || (this.x < this.ballRadius)) {
-      this.dx *= -1;      
+    if ((this.x > this.canvas.width - this.ballRadius) || (this.x <= this.ballRadius)) { 
+      this.dx *= -1; 
     }
 
-    if (this.y < this.ballRadius) {
+    if (this.y < this.ballRadius) { 
       this.dy *= -1;
-    } else if (this.y > (this.canvas.height - this.ballRadius)) {
-      this.x = this.x_i;
-      this.y = this.y_i;
-      this.dx = this.ballVelocity;
-      this.dy = -this.ballVelocity;
-    }
+    } else if (this.y > (this.canvas.height - this.ballRadius)) { 
+      this.resetBall();
+    }    
+  }
+
+  resetBall() {
+    this.x = this.platform.x_mid;
+    this.y = this.platform.y_top;
+    this.dx = this.ballVelocity;
+    this.dy = -this.ballVelocity;
   }
 
   clearBallPath() {
--- scripts/breakout.js
+++ scripts/breakout.js
@@ -1,27 +1,15 @@
 const Ball = require('./ball');
 const Platform = require('./platform');
 
+// Canvas
 const canvas = document.getElementById('canvasRoot');
   canvas.width = window.innerWidth * 0.8;
   canvas.height = (window.innerHeight) * 0.8;
 
 // Platform
-const platformWidth = 150;
-const platformHeight = 15;
-const platformX = (canvas.width - platformWidth) / 2;
-const platformY = (canvas.height - platformHeight - 15);
-
-
-
 const platform = new Platform(canvas)
 
-
 // Ball
-// const ball = new Ball(canvas, {
-//     x_i: platformX + (platformWidth/2), // Ball starts right above platform
-//     y_i: platformY - platformHeight
-//   })
-
 const ball = new Ball(canvas, platform)
 
 
