27774af5d4489be2c1d6138e51db35bf1eb7ada9
tonyjhuang <huangtonyj91@gmail.com>
2018-11-15 01:15:54 -0800

    clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,5 @@
 		    path: path.resolve(__dirname),
 		    filename: "./scripts/bundle.js"
 		  },
-		  module: {
-		    // rules: [{
-		    //   test: [/\.jsx?$/],
-		    //   exclude: /(node_modules)/,
-		    //   use: {
-		    //     loader: 'babel-loader',
-		    //     query: {
-		    //       presets: ['@babel/env', '@babel/react']
-		    //     }
-		    //   },
-		    // }]
-		  },
 		  devtool: 'source-map',
-		  // resolve: {
-		  //   extensions: ['.js', '.jsx', '*']
-		  // }
 		};
